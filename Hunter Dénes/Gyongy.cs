aef041520d8a6aa380e4839f234fbe9c17e193ce
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2024-03-20 12:57:11 +0100

    Add files via upload

---
+++ Hunter Dénes/Gyongy.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+
+namespace Hunter_Dénes;
+public class Gyongy
+{
+    public static readonly List<Gyongy> gyongyok = [];
+
+    public int X { get; init; } = 0;
+    public int Y { get; init; } = 0;
+    public int Z { get; init; } = 0;
+    public int Ertek { get; init; } = 0;
+
+    private Gyongy() { }
+    public Gyongy(int[] adatok)
+    {
+        X = adatok[0];
+        Y = adatok[1];
+        Z = adatok[2];
+        Ertek = adatok[3];
+    }
+
+    public static void Betolt(string path)
+    {
+        gyongyok.Add(new());
+
+        using (StreamReader sr = new(path))
+        {
+            sr.ReadLine();
+
+            while (sr.Peek() > 0)
+                gyongyok.Add(new(Array.ConvertAll(sr.ReadLine().TrimEnd(';').Split(';'), Convert.ToInt32)));
+
+            sr.Close();
+        }
+    }
+    public static double Távolság(Gyongy a, Gyongy b)
+    {
+        double x = Math.Pow(a.X - b.X, 2);
+        double y = Math.Pow(a.Y - b.Y, 2);
+        double z = Math.Pow(a.Z - b.Z, 2);
+
+        return Math.Sqrt(x + y + z);
+    }
+
+    public override string ToString() => $"({X};{Y};{Z}) {Ertek} Mihazánk fitying";
+}
+
+public class Robot
+{
+    public double X { get; private set; } = 0;
+    public double Y { get; private set; } = 0;
+    public double Z { get; private set; } = 0;
+
+    public void AI(Gyongy kezdopont, HashSet<Gyongy> _visited)
+    {
+        HashSet<Gyongy> visited = new(_visited) { kezdopont };
+
+        Parallel.ForEach(Gyongy.gyongyok.Except(visited), gyongy => {
+            if (!visited.Contains(gyongy))
+            {
+                DFS(gyongy, visited);
+                Console.WriteLine($"{gyongy}");
+            }
+        });
+    }
+
+    private void DFS(Gyongy kezdopont, HashSet<Gyongy> _visited, byte melyseg = 0)
+    {
+        HashSet<Gyongy> visited = new(_visited) { kezdopont };
+
+        foreach (var gyongy in Gyongy.gyongyok.Except(visited))
+        {
+            if (!visited.Contains(gyongy))
+                DFS(gyongy, visited, (byte)(melyseg + 1));
+        }
+    }
+}
---
+++ Hunter Dénes/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using HelixToolkit.Wpf;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Hunter_Dénes.Gyongy;
+using static Hunter_Dénes.Robot;
+
+namespace Hunter_Dénes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static float sebesség = 1.5f;
+
+        private float X { get; set; } = 20;
+        private float Y { get; set; } = 20;
+        private float Z { get; set; } = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Betolt("gyongyok.txt");
+
+            foreach (var gyongy in gyongyok)
+            {
+                EllipsoidVisual3D gyongy3d = new();
+                gyongy3d.RadiusX = 1;
+                gyongy3d.RadiusY = 1;
+                gyongy3d.RadiusZ = 1;
+                gyongy3d.Center = new Point3D(gyongy.X, gyongy.Y, gyongy.Z);
+                gyongy3d.Fill = new SolidColorBrush(Color.FromArgb(255, (byte)(255 - gyongy.Ertek * 20), 80, (byte)(gyongy.Ertek * 20)));
+                ter.Children.Add(gyongy3d);
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.W)
+                X += sebesség;
+            else if (e.Key == Key.S)
+                X -= sebesség;
+
+            if (e.Key == Key.LeftShift)
+                Y += sebesség;
+            else if (e.Key == Key.Space)
+                Y -= sebesség;
+
+            if (e.Key == Key.A)
+                Z += sebesség;
+            else if (e.Key == Key.D)
+                Z -= sebesség;
+
+            kamera.Position = new(X, Y, Z);
+        }
+    }
+}
