3871a8947cbf254a241e0d2d5ac8f4ec3d5407b7
Majoros Péter <majorospeterur@gmail.com>
2024-04-02 17:28:41 +0200

    új (jobb) keresés + kamera áthelyezve

--- Hunter Dénes/Classok/Robot.cs
+++ Hunter Dénes/Classok/Robot.cs
@@ -1,5 +1,7 @@
 ﻿namespace Hunter_Dénes.Classok;
 
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Controls;
 using static Gyongy;
 
@@ -7,7 +9,7 @@ public class Robot
 {
     private static double UTHOSSZ = 50;
     private static Gyongy ORIGO;
-    private static Func<Gyongy, IEnumerable<Gyongy>, double, IList<Gyongy>>? Algoritmus;
+    private static Func<Gyongy, IEnumerable<Gyongy>, double, int, IList<Gyongy>>? Algoritmus;
 
     public double X { get; private set; } = 0;
     public double Y { get; private set; } = 0;
@@ -22,6 +24,8 @@ public class Robot
         {
             "Atlagos" => AtlagosAlgoritmus,
             "Greedy" => Greedy,
+            "Gyorsitott" => GyorsabbGreedy,
+            "Hybrid" => GyorsAtlagHybrid,
             _ => DFS
         };
     }
@@ -36,7 +40,7 @@ public class Robot
             double tavolsag = ORIGO.szomszedok[gyongy.Id];
             if (tavolsag * 2 <= UTHOSSZ)
             {
-                IList<Gyongy> temp = Algoritmus(gyongy, szukitett, tavolsag);
+                IList<Gyongy> temp = Algoritmus(gyongy, szukitett, tavolsag, int.MinValue);
                 if (temp.Sum(G => G.Ertek) > optimalis.Sum(G => G.Ertek))
                     optimalis = temp;
             }
@@ -44,7 +48,7 @@ public class Robot
 
         return optimalis;
     }
-    private static IList<Gyongy> DFS(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt)
+    private static IList<Gyongy> DFS(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt, int melyseg = int.MinValue)
     {
         IList<Gyongy> optimalis = [kiindulo];
         IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
@@ -63,7 +67,7 @@ public class Robot
 
         return optimalis;
     }
-    private static IList<Gyongy> AtlagosAlgoritmus(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt)
+    private static IList<Gyongy> AtlagosAlgoritmus(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt, int melyseg = int.MinValue)
     {
         IList<Gyongy> optimalis = [kiindulo];
         IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
@@ -88,7 +92,7 @@ public class Robot
 
         return optimalis;
     }
-    private static IList<Gyongy> Greedy(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt)
+    private static IList<Gyongy> Greedy(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt, int melyseg = int.MinValue)
     {
         IList<Gyongy> optimalis = [kiindulo];
         IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
@@ -108,6 +112,76 @@ public class Robot
 
         return optimalis;
     }
+    private static IList<Gyongy> GyorsabbGreedy(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt, int melyseg = int.MinValue)
+    {
+        IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
+
+        if(!tovabbSzukitett.Any())
+            return [kiindulo];
+
+        Gyongy gyongy = tovabbSzukitett.Where(G => G.Ertek == tovabbSzukitett.Max(G => G.Ertek))
+                                       .OrderBy(G => kiindulo.szomszedok[G.Id])
+                                       .First();
+
+        return [..GyorsabbGreedy(gyongy, tovabbSzukitett, megtettUt + kiindulo.szomszedok[gyongy.Id], melyseg+1), kiindulo];
+    }
+    private static IList<Gyongy> GyorsAtlagHybrid(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt, int melyseg = int.MinValue)
+    {
+        melyseg = melyseg==int.MinValue ? 1 : melyseg;
+
+        IList<Gyongy> optimalis = [kiindulo];
+        IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
+
+        if(!tovabbSzukitett.Any())
+            return optimalis;
+
+        double atlag = tovabbSzukitett.Average(G => G.Ertek / kiindulo.szomszedok[G.Id]);
+
+        if(melyseg % 3 == 0)
+        {
+            foreach(var gyongy in tovabbSzukitett)
+            {
+                if(gyongy.Ertek / kiindulo.szomszedok[gyongy.Id] < atlag)
+                    continue;
+
+                IList<Gyongy> temp = GyorsAtlagHybrid(gyongy, tovabbSzukitett, megtettUt + kiindulo.szomszedok[gyongy.Id], melyseg+1);
+
+                if(temp.Sum(G => G.Ertek) + kiindulo.Ertek > optimalis.Sum(G => G.Ertek))
+                {
+                    temp.Add(kiindulo);
+                    optimalis = temp;
+                }
+            }
+        } else
+        {
+            Gyongy gyongy = tovabbSzukitett.Where(G => G.Ertek == tovabbSzukitett.Max(G => G.Ertek))
+                                       .OrderBy(G => kiindulo.szomszedok[G.Id])
+                                       .First();
+
+            optimalis = [.. GyorsabbGreedy(gyongy, tovabbSzukitett, megtettUt + kiindulo.szomszedok[gyongy.Id], melyseg+1), kiindulo];
+        }
+
+        return optimalis;
+
+        #region __Módosított Greedy__
+        IList<Gyongy> Greedy(Gyongy kiindulo, IEnumerable<Gyongy> szukitett, double megtettUt)
+        {
+            IEnumerable<Gyongy> tovabbSzukitett = szukitett.Where(G => G.Id != kiindulo.Id && megtettUt + kiindulo.szomszedok[G.Id] + ORIGO.szomszedok[G.Id] <= UTHOSSZ);
+
+            if(!tovabbSzukitett.Any())
+                return [kiindulo];
+
+            Gyongy gyongy = tovabbSzukitett.Where(G => G.Ertek == tovabbSzukitett.Max(G => G.Ertek))
+                                           .OrderBy(G => kiindulo.szomszedok[G.Id])
+                                           .First();
+
+            IList<Gyongy> temp = Greedy(gyongy, tovabbSzukitett, megtettUt + kiindulo.szomszedok[gyongy.Id]);
+
+            return [..temp, kiindulo];
+        }
+        #endregion
+    }
+
 
     private static IEnumerable<Gyongy> GreedySzukit(IEnumerable<Gyongy> gyongyok)
     {
